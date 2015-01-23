cbada0f49cc9eb79faf7c8eb29e26a1f0a6849fe
Navalava <navalava@gmail.com>
2015-01-22 22:25:29 -0500

    Added basic framework for PA1

---
+++ Test/src/Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double i = 4.0;
+		i = i+ 5;
+		System.out.println(i);
+		double percent = 10;
+		System.out.println(percent + "%");
+	}
+
+}
